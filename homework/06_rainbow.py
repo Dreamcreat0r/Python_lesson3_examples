fb3f09ad22bcdf51478571d43c2933b1f83b1a5a
Alexander Davydov <vetrovaikis@gmail.com>
2021-12-13 15:13:25 +0300

    done rainbow

--- homework/06_rainbow.py
+++ homework/06_rainbow.py
@@ -15,4 +15,24 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 # поэкспериментировать с параметрами, что бы было красиво
 # TODO здесь ваш код
 
+'''
+x_start = 50
+y_start = 50
+x_end = 350
+y_end = 450
+for color in rainbow_colors:
+    point_start = sd.get_point(x_start, y_start)
+    y_start += 5
+    point_end = sd.get_point(x_end, y_end)
+    y_end += 5
+    sd.line(point_start, point_end, color = color, width = 4)
+'''
+
+radius = 500
+point_round = sd.get_point(150, -200)
+for color in rainbow_colors:
+    radius += 30
+    sd.circle(point_round, radius, color, 30)
+
+
 sd.pause()
